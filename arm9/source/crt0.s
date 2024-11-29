6c0a03446c0d09576884646a2c5da8851a582a77
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-11-28 20:14:16 -0800

    Fill out remaining header values
    For the sake of correctness.
    
    nds-miniboot also needs them to be correct in order to set up the
    bootstub area.

--- arm9/source/crt0.s
+++ arm9/source/crt0.s
@@ -8,11 +8,21 @@
     .section .start, "ax"
 
 _start:
-    // set header arm7 addresses
+    // Fix NDS header values
+    ldr r1, =0x02FFFE00
+    // set header arm9 address and binary size
+    adr r0, _arm9_bin
+    str r0, [r1, #0x24]
+    str r0, [r1, #0x28]
+    mov r0, #(_arm9_bin_end - _arm9_bin)
+    str r0, [r1, #0x2C]
+
+    // set header arm7 address and binary size
     adr r0, _arm7_bin
-    ldr r1, =0x02FFFE34
-    str r0, [r1]
-    str r0, [r1, #4]
+    str r0, [r1, #0x34]
+    str r0, [r1, #0x38]
+    mov r0, #(_arm7_bin_end - _arm7_bin)
+    str r0, [r1, #0x3C]
 
     // Instruct ARM7 to reboot
     // 0x02FFF200 == ARM9 IPC message to ARM7
@@ -26,6 +36,8 @@ _start:
 
 _arm7_bin:
     .incbin "arm9/data/arm7.bin"
+_arm7_bin_end:
 
 _arm9_bin:
     .incbin "arm9/data/arm9.bin"
+_arm9_bin_end:
